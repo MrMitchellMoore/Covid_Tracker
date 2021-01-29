afd72928cde4cd1ae7a24e8ad54e71081b449e01
Mitchell Moore <mmoorejr24@gmail.com>
2021-01-28 16:44:02 -0800

    First commit and cleaned and updated app.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React from 'react';
-import './App.css';
+import {Cards, Chart, CountryPicker} from './components';
+import styles from './App.module.css';
+import {fetchData} from './api';
 
-function App () {
-  return <div className="App"> Hello There</div>;
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  };
+  async componentDidMount () {
+    const fetchedData = await fetchData ();
+    this.setState ({data: fetchedData});
+  }
+
+  handleCountryChange = async country => {
+    const data = await fetchData (country);
+
+    this.setState ({data, country: country});
+  };
+
+  render () {
+    const {data, country} = this.state;
+    return (
+      <div className={styles.container}>
+        <h1>COVID-19 Tracker 2020</h1>
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
 }
 
 export default App;
